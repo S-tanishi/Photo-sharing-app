9d65274c8a24edd7c011e48f0c73ddc257e2c259
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-25 14:14:19 +0900

    add:ルーティング

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//APIのURL以外のリクエストに関してはindexテンプレートを返す
+//画面遷移はフロントエンドのVueRouterが制御
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
