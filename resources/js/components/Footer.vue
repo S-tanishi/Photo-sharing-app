731d4ed87bde031452fafd3b01f4fc208d4b5db0
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-26 16:35:38 +0900

    add:footer-component

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="footer">
+        <button class="button button--link">Logout</button>
+        <RouteLink class="button button--link" to="/login">
+          Login / Register
+        </RouteLink>
+    </footer>
+</template>
