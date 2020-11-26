42116706e422d84100160e9466cdb9bad43669e5
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-26 16:24:18 +0900

    add:navbar-component

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar">
+        <RouteLink class="navbar__brand" to="/">
+          Vuesplash
+        </RouteLink>
+        <div class="navbar__memu">
+            <div class="navbar__item">
+                <button class="button">
+                    <i class="icon" ion-md-add></i>
+                    Submit a photo
+                </button>
+            </div>
+            <span class="navbar__item">
+                username 
+            </span>
+            <div class="navbar__item">
+                <RouteLiink class="button button--link" to="/login">
+                  Login / register
+                </RouteLiink>
+            </div>
+        </div>
+    </nav>
+</template>
