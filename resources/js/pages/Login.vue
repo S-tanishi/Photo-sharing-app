87d520ded4ed47f1d722268dcd3241c8847245f9
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-27 15:20:09 +0900

    add:会員登録フォーム

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -25,8 +25,23 @@
                     <button type="submit" class="button button--inverse">login</button>
                 </div>
             </form>
-            
-        <div class="panel" v-show="tab === 2">Register Form</div>login</button>
+        <!-- 会員登録フォーム -->
+        <div class="panel" v-show="tab === 2">
+            <form class="form" @submit.prevent="register">
+                <label for="username">Name</label>
+                <input type="text" class="form__item" id="username" v-model="registerForm.name">
+                
+                <label for="email">Email</label>
+                <input type="text" class="form__item" id="email" v-model="registerForm.email">
+                
+                <label for="password">Password</label>
+                <input type="password" class="form__item" id="password" v-model="registerForm.password">
+                
+                <div class="form__button">
+                    <button type="submit" class="button button--inverse">register</button>
+                </div>
+            </form>
+        </div>
     </div>              
 </template>
 
