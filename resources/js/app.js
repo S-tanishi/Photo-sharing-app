0a87238a5c3c63ed7dd8941ca15dd651c61dc18c
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-27 15:54:40 +0900

    add:ストアの読み取り処理

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,12 @@ import router from './router'
 //ルートコンポーネントをインポートする
 import App from './App.vue'
 
+import store from './store'
+
 new Vue({
     el: '#app',
     router, //ルーティングの定義を読み込む
+    store, // インスタンス作成時にストアの読み取り
     components: { App }, //ルートコンポーネントの使用を宣言する
     template: '<App />'
 })
