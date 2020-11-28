877cfd84183c1d52f157dffc512507cb1989d607
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-28 14:04:36 +0900

    add:ログイン ストアの作成

--- public/js/app.js
+++ public/js/app.js
@@ -37369,7 +37369,7 @@ var actions = {
           switch (_context.prev = _context.next) {
             case 0:
               _context.next = 2;
-              return axios__WEBPACK_IMPORTED_MODULE_1___default.a.post('/api/register', data);
+              return axios.post('/api/register', data);
 
             case 2:
               response = _context.sent;
@@ -37381,6 +37381,28 @@ var actions = {
           }
         }
       }, _callee);
+    }))();
+  },
+  login: function login(content, data) {
+    return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee2() {
+      var response;
+      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee2$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              _context2.next = 2;
+              return axios.post('/api/login', data);
+
+            case 2:
+              response = _context2.sent;
+              context.commit('setUser', response.data);
+
+            case 4:
+            case "end":
+              return _context2.stop();
+          }
+        }
+      }, _callee2);
     }))();
   }
 };
--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -15,7 +15,11 @@ const mutations = {
 const actions = {
     // 会員登録 API を呼び出す register アクション
     async register (context, data) {
-        const response = await Axios.post('/api/register', data)
+        const response = await axios.post('/api/register', data)
+        context.commit('setUser', response.data)
+    },
+    async login (content, data) {
+        const response = await axios.post('/api/login', data)
         context.commit('setUser', response.data)
     }
 }
