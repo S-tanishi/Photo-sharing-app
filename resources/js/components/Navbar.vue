39a3560df79af7faded5c5c8442bf32ae0a542e4
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-28 21:36:06 +0900

    add;ナビゲーションバーの要素出し分け

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -4,16 +4,16 @@
           Vuesplash
         </RouteLink>
         <div class="navbar__memu">
-            <div class="navbar__item">
+            <div v-if="isLogin" class="navbar__item">
                 <button class="button">
                     <i class="icon" ion-md-add></i>
                     Submit a photo
                 </button>
             </div>
-            <span class="navbar__item">
+            <span v-if="isLogin" class="navbar__item">
                 username 
             </span>
-            <div class="navbar__item">
+            <div v-else class="navbar__item">
                 <RouteLiink class="button button--link" to="/login">
                   Login / register
                 </RouteLiink>
