fe90aad38ed6a64ea54d48c8a8c08425c9d97065
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-11-28 19:59:05 +0900

    add:logout ストア

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -23,6 +23,11 @@ const actions = {
         const response = await axios.post('/api/login', data)
         context.commit('setUser', response.data)
     }
+    // ログアウト
+    async logout (context) {
+        const response = await axios.post('/api/logout')
+        context.commit('setUser', null)
+    }
 }
 
 export default {
