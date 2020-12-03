88acff3c9f1d6154a1bb1b1c2f114c434b6f0542
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-03 14:48:59 +0900

    add:test 投稿機能API

--- tests/Feature/PhotoSubmitApiTest.php
+++ tests/Feature/PhotoSubmitApiTest.php
@@ -5,18 +5,43 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facade\Storage;
 
 class PhotoSubmitApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+    /**
+     * @test
+     */
+    public function should_ファイルをアップロードできる()
+    {
+        // S3ではなくテスト用のストレージを使用
+        // → storage/framework/testing
+        Storage::fake('s3');
+
+        $response = $this->actingAs($this->user)
+           ->json('POST', route('photo.create'), [
+               // ダミーファイルを作成して送信している
+               'photo' => UploadedFile::fake()->image('photo.jpg'),
+           ]);
+        
+        // レスポンスが201(CREATED)であること
+        $response->assertStatus(201);
+
+        $photo = Photo::first();
+
+        // 写真のIDが12桁のランダムな文字列であること
+        $this->assertRegExp('/^[0-9a-zA-Z-_]{12}$/', $photo->id);
+
+        // DBに挿入されたファイル名のファイルがストレージに保存されていること
+        Storage::cloud()->assertExists($photo->filename);
     }
 }
