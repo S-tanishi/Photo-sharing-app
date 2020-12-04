5752db14e8e4028af93a96893cdb87641b8cd118
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-04 16:00:10 +0900

    add:photo-form composer 表示・非表示の切り替え機能

--- resources/js/components/PhotoForm.vue
+++ resources/js/components/PhotoForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="photo-form">
+    <div v-show="value" class="photo-form">
         <h2 class="title">Submit a photp</h2>
         <form class="form">
             <input class="form__item" type="file">
@@ -9,3 +9,14 @@
         </form>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Boolean,
+            required: true
+        }
+    }
+}
+</script>
