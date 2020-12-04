2cfdcd53fd7b5ede12cda737ada31dca6eb97d6d
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-04 15:06:19 +0900

    add:photo-コントローラーの作成

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\StorePhoto;
+use App\Models\Photo;
+use Illuminate\Support\Facades\Storage;
+
+class PhotoController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * 写真投稿機能
+     */
+    public function create(StorePhoto $request)
+    {
+        // 投稿写真の拡張子を取得
+        $extension = $request->photo->extension();
+
+        $photo = new Photo();
+
+        // インスタンス生成時に割り振られたランダムなID値と
+        // 本来の拡張子を組み合わせてファイル名とする
+        $photo->filename = $photo->id . '.' . $extension;
+
+        // $3にファイルを保存する
+        // 第３引数の'public'はファイルを公開状態で保存するため
+        Storage::cloud()
+            ->putFileAs('', $request->photo, $photo->filename, 'public');
+        
+        // DBエラー時にファイル削除を行うため
+        // トランザクションを利用
+        DB::beginTransaction();
+
+        try {
+            Auth::user()->photos()->save($photo);
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            // DBとの不整合を避けるためアップロードしたファイルを削除
+            Storage::cloud()->delete($photo->filename);
+            throw $exception;
+        }
+
+        // リソースの新規作成なので
+        // レスポンスコードは201(CREATED)を返却する
+        return response($photo, 201);
+    }
+    }
+}
