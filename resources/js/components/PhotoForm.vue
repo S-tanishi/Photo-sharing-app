ae9b02b891581b9a92ae500ab31b3a2fd774a4c8
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-04 15:52:59 +0900

    add:ファイルコンポーネントを作成

---
+++ resources/js/components/PhotoForm.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="photo-form">
+        <h2 class="title">Submit a photp</h2>
+        <form class="form">
+            <input class="form__item" type="file">
+            <div class="form_button">
+                <button type="submit" class="button--inverse">submit</button>
+            </div>
+        </form>
+    </div>
+</template>
