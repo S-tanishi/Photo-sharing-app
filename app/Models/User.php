d55d0ed7eaf71917bd8bfc3d7eeafffe546ee731
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-06 17:39:35 +0900

    add:JSON に表示させる必要のない項目を隠す

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,17 +21,6 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
     /**
      * The attributes that should be cast to native types.
      *
@@ -39,6 +28,9 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+    protected $visible = [
+        'name',
     ];
     /**
      * リレーションシップ - photosテーブル
