c9fda57c2cf6cd6eb6c10ff9f8ca4de65723788f
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-12-09 09:29:50 +0900

    fix:ログイン syntax修正

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PhotoController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,26 +21,36 @@ use App\Http\Controllers\PhotoController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+// 会員登録
 Route::post('/register', 'RegisterController@register')->name('register');
 
+// ログイン
 Route::post('/login', 'LoginController@login')->name('login');
-//ログアウト
-Route::post('/logout', 'LoginController@logout')->name('logout');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
 // ログインユーザー
-Route::get('user', fn() => Auth::user())->name('user');
+Route::get('/user', fn() => Auth::user())->name('user');
+
 // 写真投稿
 Route::post('/photos', 'PhotoController@create')->name('photo.create');
+
 // 写真一覧
 Route::get('/photos', 'PhotoController@index')->name('photo.index');
+
 // 写真詳細
 Route::get('/photos/{id}','PhotoController@show')->name('photo.show');
+
 // コメント
 Route::post('/photos/{photo}/comments', 'PhotoController@addComment')->name('photo.comment');
+
 // いいね
 Route::put('/photos/{id}/like', 'PhotoController@like')->name('Photo.like');
+
 // いいね解除
 Route::delete('/photos/{id}/like', 'PhotoController@unlike');
+
 // トークンリフレッシュ(CSRF トークンがうまくリフレッシュされず、再ログインできないため)
 Route::get('/reflesh-token', function (Illuminate\Http\Request $request) {
     $request->session()->regenerateToken();
